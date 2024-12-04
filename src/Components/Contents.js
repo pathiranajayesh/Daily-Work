5a578230c21957aac6c832d5448c05d5dce77dcf
Praneeth Jayesha <pathiranajayesh@gmail.com>
2024-12-04 20:02:43 +0530

    First Commit

---
+++ src/Components/Contents.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './contents.css'
+
+export default function content() {
+  return (
+    <div className='paragraph'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quidem natus repellendus enim ut, tempore fuga. Explicabo aspernatur quo ullam, nostrum adipisci ad dolorem similique at blanditiis ipsa dignissimos ipsam!Lorem  ipsum dolor sit amet, consectetur adipisicing elit. Dolor quidem natus repellendus enim ut, tempore fuga. Explicabo aspernatur quo ullam, nostrum adipisci ad dolorem similique at blanditiis ipsa dignissimos ipsam!Lorem</p>
+    </div>
+  )
+}
---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TopicBox from '../Components/TopicBox'
+import Contents from '../Components/Contents'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <TopicBox food="Kottu" price = "Rs.250"/>
+      <TopicBox food="Rice" price = "Rs.300"/>
+      <TopicBox food="Hoppers" price = "Rs.450"/>
+      {/* <Contents/> */}
+      
+    </div>
+  )
+}
