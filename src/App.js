942faad37f07210abcd3ef35a8a15b765e40a409
Praneeth Jayesha <pathiranajayesh@gmail.com>
2024-12-09 17:15:42 +0530

    Use Effect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Profile from './Profile';
-import About from './About';
+import { Button } from 'bootstrap';
 
 
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  //To Increment
+  const increment = ()=>{
+    setCount(count+1);
+  };
+
+  //To Decrement
+  const decrement=()=>{
+    setCount(count-1);
+  };
+
+  useEffect(()=>{
+    //What I want to do as a side Effect(Mokkd wenne kiyala kiyanna ona methanadi)
+    console.log(`New Count is :${count}`)
+  },[count]//Dependency Array(Mokk diha balagenada ude {} athule theena de karanna ona kiyala liyanne methandi)
+
+
+
+  )
+
   return (
     <div>
+        <h1>Count:{count}</h1>
+        <button onClick = {increment}>+</button>
+        <button onClick= {decrement}>-</button>
 
-      <h1>Static Content</h1>
-
-      <BrowserRouter>
-        <Routes>
-          
-            <Route path="/" element={<Home/>}>
-            </Route>
-
-            <Route path='/profile' element={<Profile/>}>
-            </Route>
-
-            <Route path='/about' element={<About/>}>
-            </Route>
-
-        </Routes>
-      </BrowserRouter>
     </div>
   )
 
